170509162cb77ef792a08a96fe4b4088dbb6b401
yaredar <yaredararso3@gmail.com>
2026-01-25 09:34:19 +0300

    Add files via upload

---
+++ lib/utils.ts
@@ -0,0 +1,10 @@
+
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * Standard utility for merging tailwind classes with conditional logic.
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
